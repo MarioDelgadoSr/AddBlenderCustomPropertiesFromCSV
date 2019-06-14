ec101cb09609d3627952358fe460faf4f266e094
Mario Delgado <51416273+MarioDelgadoSr@users.noreply.github.com>
2019-06-13 22:43:30 -0400

    Add Blender Custom Properties From CSV

--- AddBlenderCustomPropertiesFromCSV.py
+++ AddBlenderCustomPropertiesFromCSV.py
@@ -1,21 +1,3 @@
-<!-- Markdown reference: https://guides.github.com/features/mastering-markdown/ -->
-# Add Blender Custom Properties From CSV File
-
-A Blender Python script to read a formatted csv (comma seperated value) file to set [Custom Properties](https://docs.blender.org/manual/en/latest/data_system/custom_properties.html?highlight=custom%20properties).
-
-# Usage
-
-[Run script from in Blender](https://docs.blender.org/api/2.79/info_quickstart.html#running-scripts) and respond to the input prompt with filename.csv that contains the data to update the Custom Properies. 
-
-* Assuming a Blender object (.type = 'MESH') with a name = 'object1', the following file contents will assign the object the value of 'prop1' to property 'propName1'
-
-objectName,propName1
-object1,prop1
-
-Script Syntax:
-
-''''
-
 # Python script to set Blender Custom Properties for a mesh (.type == 'MESH')
 # Author: Mario Delgado, LinkeIn: https://www.linkedin.com/in/mario-delgado-5b6195155/
 # Source: http://github.com/MarioDelgadoSr/AddBlenderCustomPropertiesFromCSV
@@ -54,19 +36,3 @@ with open( filePath ) as csvfile:
         
         print(" properties after assignment(s): ", bpy.data.objects[meshName].data.items()) 
         print("******************************** meshName:", meshName ,"********************************************")
-
-
-''''
-
-
-## Author
-
-* **Mario Delgado**  Github: [MarioDelgadoSr](https://github.com/MarioDelgadoSr)
-* LinkedIn: [Mario Delgado](https://www.linkedin.com/in/mario-delgado-5b6195155/)
-* [My Data Visualizer](https://qzfcxunzx7ydnpxm3djoqw-on.drv.tw/DataVisualizer/): A data visualization application using the *DataVisual* design pattern.
-
-
-## License
-
-This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
-
